60dc6e5f923786216075d280204809be77058dee
Alexandre-Jacques St-Jacques <alexandre.st-jacques@groupegodin.com>
2016-05-05 22:08:35 -0400

    Ajout d'un exception lors d'un login invalide

---
+++ OrDragon/OrDragon/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrDragon.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public InvalidLoginException()
+        {
+        }
+
+        public InvalidLoginException(string message) : base(message)
+        {
+        }
+
+        public InvalidLoginException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
